7e28a239a021bdbf87ee261b60171bdb50803dfc
Judzhin Miles <info@msbios.com>
2017-11-17 10:42:41 +0200

    Lazy Composer Commiter

--- config/module.config.php
+++ config/module.config.php
@@ -5,7 +5,18 @@
  */
 namespace MSBios\Authentication;
 
+use Zend\Authentication\AuthenticationService;
+use Zend\ServiceManager\Factory\InvokableFactory;
+
 return [
+
+    'service_manager' => [
+
+        'factories' => [
+            AuthenticationService::class =>
+                InvokableFactory::class
+        ]
+    ],
 
     Module::class => [
 
